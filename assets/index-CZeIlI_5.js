bd41ffcc48c3d59a941824721190f2c2fc6f3a5e
mmy <mmy@qq.com>
2025-09-28 11:29:31 +0800

    1

--- assets/index-CZeIlI_5.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as r}from"./index-MGEoj_4B.js";const t=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"用户管理"}),s.jsx("p",{children:"这是用户管理页面"}),s.jsx(r,{})]});export{t as default};
