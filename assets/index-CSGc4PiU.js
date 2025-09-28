3589c903596d3cbc3a440d6fee4426967c854685
mmy <mmy@qq.com>
2025-09-28 13:57:52 +0800

    1

--- assets/index-CSGc4PiU.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as t}from"./index-CbQUCMqF.js";const r=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"系统管理"}),s.jsx("p",{children:"这是系统管理页面"}),s.jsx(t,{})]});export{r as default};
