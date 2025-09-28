dc627bc9c8a28f345df68a7d2c1610fe8214205b
mmy <mmy@qq.com>
2025-09-28 13:49:06 +0800

    7

---
+++ assets/index-DKFWhit-.js
@@ -0,0 +1,1 @@
+import{j as s,O as r}from"./index-DmxoTSWn.js";const t=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"用户管理"}),s.jsx("p",{children:"这是用户管理页面"}),s.jsx(r,{})]});export{t as default};
